33299356c132976b96b9f2f26f3caa27201e0ba9
johnabass <johnbass1017@yahoo.com>
2019-07-23 13:32:03 -0700

    Better comments; made random optional since we have a useful default; container tests

--- src/key/provide.go
+++ src/key/provide.go
@@ -6,19 +6,27 @@ import (
 	"go.uber.org/fx"
 )
 
+// KeyIn is the set of dependencies for this package's components
 type KeyIn struct {
 	fx.In
 
-	Random io.Reader
+	// Random is the optional source of randomness.  If not present in the container,
+	// crypto/rand.Reader is used.
+	Random io.Reader `optional:"true"`
 }
 
+// KeyOut is the set of components emitted by this package
 type KeyOut struct {
 	fx.Out
 
+	// Registry is the fully configured token Registry
 	Registry Registry
-	Handler  Handler
+
+	// Handler is the http.Handler which can serve key requests to the Registry
+	Handler Handler
 }
 
+// Provide is an uber/fx style provider for this package's components
 func Provide(in KeyIn) KeyOut {
 	registry := NewRegistry(in.Random)
 
---
+++ src/key/provide_test.go
@@ -0,0 +1,59 @@
+package key
+
+import (
+	"random"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+)
+
+func TestProvide(t *testing.T) {
+	t.Run("WithCustomRandom", func(t *testing.T) {
+		var (
+			assert = assert.New(t)
+
+			registry Registry
+			handler  Handler
+
+			app = fxtest.New(
+				t,
+				fx.Provide(
+					random.Provide,
+					Provide,
+				),
+				fx.Populate(&registry, &handler),
+			)
+		)
+
+		app.RequireStart()
+		assert.NotNil(registry)
+		assert.NotNil(handler)
+
+		app.RequireStop()
+	})
+
+	t.Run("WithDefaultRandom", func(t *testing.T) {
+		var (
+			assert = assert.New(t)
+
+			registry Registry
+			handler  Handler
+
+			app = fxtest.New(
+				t,
+				fx.Provide(
+					Provide,
+				),
+				fx.Populate(&registry, &handler),
+			)
+		)
+
+		app.RequireStart()
+		assert.NotNil(registry)
+		assert.NotNil(handler)
+
+		app.RequireStop()
+	})
+}
