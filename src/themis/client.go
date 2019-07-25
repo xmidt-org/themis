5c6547434791a6e2a8b79153c02906e377e82db2
johnabass <johnbass1017@yahoo.com>
2019-07-25 15:37:04 -0700

    simplified logic

--- src/themis/client.go
+++ src/themis/client.go
@@ -14,17 +14,11 @@ type ClientIn struct {
 
 func provideClient(configKey string) func(ClientIn) (xhttpclient.Interface, error) {
 	return func(in ClientIn) (xhttpclient.Interface, error) {
-		c, err := xhttpclient.Unmarshal(
+		return xhttpclient.Unmarshal(
 			configKey,
 			in.RequestCount.Then,
 			in.RequestDuration.Then,
 			in.RequestsInFlight.Then,
 		)(in.UnmarshalIn)
-
-		if err != nil {
-			return nil, err
-		}
-
-		return c, nil
 	}
 }
