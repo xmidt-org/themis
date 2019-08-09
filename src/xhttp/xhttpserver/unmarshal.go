00f5a2ac53b05ab0d0038022ddc43d3832a20957
johnabass <johnbass1017@yahoo.com>
2019-08-08 21:35:18 -0700

    Support both a singleton server and multiple named servers

--- src/xhttp/xhttpserver/unmarshal.go
+++ src/xhttp/xhttpserver/unmarshal.go
@@ -50,35 +50,49 @@ func unmarshal(configKey string, in ServerIn) (*mux.Router, error) {
 	return router, nil
 }
 
-// Required unmarshals a server from the given configuration key and emits a *mux.Router
-// with the same name as the key.  This provider raises an error if the configuration key does not exist.
-func Required(configKey string) fx.Annotated {
+// Required unmarshals a server from the given configuration key and emits a *mux.Router.
+// This provider raises an error if the configuration key does not exist.
+func Required(configKey string) func(in ServerIn) (*mux.Router, error) {
+	return func(in ServerIn) (*mux.Router, error) {
+		return unmarshal(configKey, in)
+	}
+}
+
+// NamedRequired unmarshals a server and emits its *mux.Router as a component with the same
+// name as the configuration key.  This is useful when an application starts multiple servers.
+func NamedRequired(configKey string) fx.Annotated {
 	return fx.Annotated{
-		Name: configKey,
-		Target: func(in ServerIn) (*mux.Router, error) {
-			return unmarshal(configKey, in)
-		},
+		Name:   configKey,
+		Target: Required(configKey),
 	}
 }
 
 // Optional unmarshals a server from the given configuration key, returning a nil *mux.Router if
 // no such configuration key is found.
-func Optional(configKey string) fx.Annotated {
+func Optional(configKey string) func(in ServerIn) (*mux.Router, error) {
+	return func(in ServerIn) (*mux.Router, error) {
+		r, err := unmarshal(configKey, in)
+		if _, ok := err.(config.MissingKeyError); ok {
+			in.Logger.Log(
+				level.Key(), level.InfoValue(),
+				"configKey", configKey,
+				xlog.MessageKey(), "server not configured",
+			)
+
+			return nil, nil
+		}
+
+		return r, err
+	}
+}
+
+// NamedOptional unmarshals a server and emits its *mux.Router as a component with the same
+// name as the configuration key.  This is useful when an application starts multiple servers.
+//
+// As with Optional, this provider emits a nil *mux.Router if the configuration key is not present.
+func NamedOptional(configKey string) fx.Annotated {
 	return fx.Annotated{
-		Name: configKey,
-		Target: func(in ServerIn) (*mux.Router, error) {
-			r, err := unmarshal(configKey, in)
-			if _, ok := err.(config.MissingKeyError); ok {
-				in.Logger.Log(
-					level.Key(), level.InfoValue(),
-					"configKey", configKey,
-					xlog.MessageKey(), "server not configured",
-				)
-
-				return nil, nil
-			}
-
-			return r, err
-		},
+		Name:   configKey,
+		Target: Optional(configKey),
 	}
 }
