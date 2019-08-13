8a1e38f33f1edd05f9179e796917c333628e72f1
johnabass <johnbass1017@yahoo.com>
2019-08-13 14:22:38 -0700

    TrackingWriter tests

---
+++ src/xhttp/xhttpserver/mocks_test.go
@@ -0,0 +1,112 @@
+package xhttpserver
+
+import (
+	"bufio"
+	"net"
+	"net/http"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type mockResponseWriter struct {
+	mock.Mock
+}
+
+func (m *mockResponseWriter) Header() http.Header {
+	var (
+		arguments = m.Called()
+		first, _  = arguments.Get(0).(http.Header)
+	)
+
+	return first
+}
+
+func (m *mockResponseWriter) ExpectHeader() *mock.Call {
+	return m.On("Header")
+}
+
+func (m *mockResponseWriter) Write(b []byte) (int, error) {
+	arguments := m.Called(b)
+	return arguments.Int(0), arguments.Error(1)
+}
+
+func (m *mockResponseWriter) ExpectWrite(b []byte) *mock.Call {
+	return m.On("Write", b)
+}
+
+func (m *mockResponseWriter) WriteHeader(statusCode int) {
+	m.Called(statusCode)
+}
+
+func (m *mockResponseWriter) ExpectWriteHeader(statusCode int) *mock.Call {
+	return m.On("WriteHeader", statusCode)
+}
+
+type mockHijacker struct {
+	mock.Mock
+}
+
+func (m *mockHijacker) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	var (
+		arguments = m.Called()
+		first, _  = arguments.Get(0).(net.Conn)
+		second, _ = arguments.Get(1).(*bufio.ReadWriter)
+	)
+
+	return first, second, arguments.Error(2)
+}
+
+func (m *mockHijacker) ExpectHijack() *mock.Call {
+	return m.On("Hijack")
+}
+
+type mockFlusher struct {
+	mock.Mock
+}
+
+func (m *mockFlusher) Flush() {
+	m.Called()
+}
+
+func (m *mockFlusher) ExpectFlush() *mock.Call {
+	return m.On("Flush")
+}
+
+type mockPusher struct {
+	mock.Mock
+}
+
+func (m *mockPusher) Push(target string, opts *http.PushOptions) error {
+	return m.Called(target, opts).Error(0)
+}
+
+func (m *mockPusher) ExpectPush(target string, opts *http.PushOptions) *mock.Call {
+	return m.On("Push", target, opts)
+}
+
+type mockStatusCoder struct {
+	mock.Mock
+}
+
+func (m *mockStatusCoder) StatusCode() int {
+	return m.Called().Int(0)
+}
+
+func (m *mockStatusCoder) ExpectedStatusCode() *mock.Call {
+	return m.On("StatusCode")
+}
+
+type hijackerWriter struct {
+	http.ResponseWriter
+	http.Hijacker
+}
+
+type pusherWriter struct {
+	http.ResponseWriter
+	http.Pusher
+}
+
+type flusherWriter struct {
+	http.ResponseWriter
+	http.Flusher
+}
---
+++ src/xhttp/xhttpserver/trackingWriter_test.go
@@ -0,0 +1,242 @@
+package xhttpserver
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testTrackingWriterBasic(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		next = new(mockResponseWriter)
+		tr   = NewTrackingWriter(next)
+
+		actualHeader   = make(http.Header)
+		expectedHeader = http.Header{"X-Test": []string{"value1", "value2"}}
+
+		firstWrite  = []byte("test")
+		secondWrite = []byte("test again")
+
+		expectedWriteErr = errors.New("expected write error")
+	)
+
+	require.NotNil(tr)
+	assert.Zero(tr.BytesWritten())
+	assert.False(tr.Hijacked())
+	assert.Equal(http.StatusOK, tr.StatusCode())
+
+	next.ExpectHeader().Twice().Return(actualHeader)
+	next.ExpectWrite(firstWrite).Once().Return(len(firstWrite), error(nil))
+	next.ExpectWrite(secondWrite).Once().Return(0, expectedWriteErr)
+	next.ExpectWriteHeader(299).Once()
+
+	tr.Header().Add("X-Test", "value1")
+	tr.Header().Add("X-Test", "value2")
+
+	c, err := tr.Write(firstWrite)
+	assert.Equal(len(firstWrite), c)
+	assert.NoError(err)
+
+	c, err = tr.Write(secondWrite)
+	assert.Zero(c)
+	assert.Equal(expectedWriteErr, err)
+
+	tr.WriteHeader(299)
+
+	assert.Equal(299, tr.StatusCode())
+	assert.Equal(expectedHeader, actualHeader)
+
+	assert.Equal(len(firstWrite), tr.BytesWritten())
+	assert.False(tr.Hijacked())
+	next.AssertExpectations(t)
+}
+
+func testTrackingWriterHijack(t *testing.T) {
+	t.Run("ImplementsHijacker", func(t *testing.T) {
+		var (
+			assert  = assert.New(t)
+			require = require.New(t)
+
+			next     = new(mockResponseWriter)
+			hijacker = new(mockHijacker)
+			tr       = NewTrackingWriter(hijackerWriter{
+				ResponseWriter: next,
+				Hijacker:       hijacker,
+			})
+
+			expectedConn = new(net.IPConn)
+			expectedRW   = new(bufio.ReadWriter)
+		)
+
+		require.NotNil(tr)
+		assert.False(tr.Hijacked())
+		hijacker.ExpectHijack().Once().Return(expectedConn, expectedRW, error(nil))
+
+		actualConn, actualRW, err := tr.Hijack()
+		assert.Equal(expectedConn, actualConn)
+		assert.Equal(expectedRW, actualRW)
+		assert.NoError(err)
+
+		assert.True(tr.Hijacked())
+		next.AssertExpectations(t)
+		hijacker.AssertExpectations(t)
+	})
+
+	t.Run("DoesNotImplementHijacker", func(t *testing.T) {
+		var (
+			assert  = assert.New(t)
+			require = require.New(t)
+
+			next = new(mockResponseWriter)
+			tr   = NewTrackingWriter(next)
+		)
+
+		require.NotNil(tr)
+		assert.False(tr.Hijacked())
+
+		c, rw, err := tr.Hijack()
+		assert.Nil(c)
+		assert.Nil(rw)
+		assert.Equal(ErrHijackerNotSupported, err)
+
+		assert.False(tr.Hijacked())
+		next.AssertExpectations(t)
+	})
+}
+
+func testTrackingWriterPush(t *testing.T) {
+	t.Run("ImplementsPusher", func(t *testing.T) {
+		var (
+			assert  = assert.New(t)
+			require = require.New(t)
+
+			next   = new(mockResponseWriter)
+			pusher = new(mockPusher)
+			tr     = NewTrackingWriter(pusherWriter{
+				ResponseWriter: next,
+				Pusher:         pusher,
+			})
+
+			expectedTarget      = "test"
+			expectedPushOptions = &http.PushOptions{Method: "POST"}
+			expectedErr         = errors.New("expected push error")
+		)
+
+		require.NotNil(tr)
+		pusher.ExpectPush(expectedTarget, expectedPushOptions).Once().Return(expectedErr)
+
+		actualErr := tr.Push(expectedTarget, expectedPushOptions)
+		assert.Equal(expectedErr, actualErr)
+
+		next.AssertExpectations(t)
+		pusher.AssertExpectations(t)
+	})
+
+	t.Run("DoesNotImplementPusher", func(t *testing.T) {
+		var (
+			assert  = assert.New(t)
+			require = require.New(t)
+
+			next = new(mockResponseWriter)
+			tr   = NewTrackingWriter(next)
+		)
+
+		require.NotNil(tr)
+
+		err := tr.Push("test", new(http.PushOptions))
+		assert.Equal(http.ErrNotSupported, err)
+
+		next.AssertExpectations(t)
+	})
+}
+
+func testTrackingWriterFlush(t *testing.T) {
+	t.Run("ImplementsFlusher", func(t *testing.T) {
+		var (
+			require = require.New(t)
+
+			next    = new(mockResponseWriter)
+			flusher = new(mockFlusher)
+			tr      = NewTrackingWriter(flusherWriter{
+				ResponseWriter: next,
+				Flusher:        flusher,
+			})
+		)
+
+		require.NotNil(tr)
+		flusher.ExpectFlush().Once()
+
+		tr.Flush()
+
+		next.AssertExpectations(t)
+		flusher.AssertExpectations(t)
+	})
+
+	t.Run("DoesNotImplementFlusher", func(t *testing.T) {
+		var (
+			require = require.New(t)
+
+			next = new(mockResponseWriter)
+			tr   = NewTrackingWriter(next)
+		)
+
+		require.NotNil(tr)
+		tr.Flush()
+		next.AssertExpectations(t)
+	})
+}
+
+func TestTrackingWriter(t *testing.T) {
+	t.Run("Basic", testTrackingWriterBasic)
+	t.Run("Hijack", testTrackingWriterHijack)
+	t.Run("Push", testTrackingWriterPush)
+	t.Run("Flush", testTrackingWriterFlush)
+}
+
+func TestNewTrackingWriter(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+		next    = new(mockResponseWriter)
+	)
+
+	tr := NewTrackingWriter(next)
+	require.NotNil(tr)
+
+	next.ExpectHeader().Once().Return(http.Header{"X-Test": []string{"value"}})
+	assert.Equal(
+		http.Header{"X-Test": []string{"value"}},
+		tr.Header(),
+	)
+
+	assert.Equal(tr, NewTrackingWriter(tr))
+	next.AssertExpectations(t)
+}
+
+func TestUseTrackingWriter(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		next = http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+			assert.Implements((*TrackingWriter)(nil), response)
+			response.WriteHeader(299)
+		})
+
+		handler  = UseTrackingWriter(next)
+		response = httptest.NewRecorder()
+	)
+
+	require.NotNil(handler)
+	handler.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(299, response.Code)
+}
