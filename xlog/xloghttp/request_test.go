73edb5d31ca148032a76d1d681982d1e95315709
Serena Zamarripa <rena.zamarripa@gmail.com>
2022-10-03 11:43:20 -0500

    Fix staticcheck issue

--- xlog/xloghttp/request_test.go
+++ xlog/xloghttp/request_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/xmidt-org/themis/xlog"
 
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
