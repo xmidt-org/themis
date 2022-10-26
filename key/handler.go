e92f474a107869e997f8067dc2feca0a7354b00b
Owen Cabalceta <denopink@gmail.com>
2022-10-26 10:55:46 -0400

    update key package

--- key/handler.go
+++ key/handler.go
@@ -5,11 +5,11 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/xmidt-org/themis/xlog"
+	"github.com/xmidt-org/sallust"
+	"go.uber.org/zap"
 
 	"github.com/go-kit/kit/endpoint"
 	kithttp "github.com/go-kit/kit/transport/http"
-	"github.com/go-kit/log/level"
 	"github.com/gorilla/mux"
 )
 
@@ -33,12 +33,9 @@ func NewHandler(e endpoint.Endpoint) Handler {
 				return nil, ErrNoKidVariable
 			}
 
-			xlog.Get(ctx).Log(
-				level.Key(), level.InfoValue(),
-				xlog.MessageKey(), "key request",
-				"kid", kid,
+			sallust.Get(ctx).Info("key request",
+				zap.String("kid", kid),
 			)
-
 			return kid, nil
 		},
 		func(_ context.Context, response http.ResponseWriter, value interface{}) error {
@@ -60,10 +57,8 @@ func NewHandlerJWK(e endpoint.Endpoint) Handler {
 				return nil, ErrNoKidVariable
 			}
 
-			xlog.Get(ctx).Log(
-				level.Key(), level.InfoValue(),
-				xlog.MessageKey(), "key request jwk",
-				"kid", kid,
+			sallust.Get(ctx).Info("key request jwk",
+				zap.String("kid", kid),
 			)
 
 			return kid, nil
