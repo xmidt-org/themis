124d9a8f09f872209204b555e05ec7dcffe862ef
Owen Cabalceta <denopink@gmail.com>
2022-10-26 16:27:10 -0400

    update xhealth package

--- xhealth/health.go
+++ xhealth/health.go
@@ -5,9 +5,7 @@ import (
 	"errors"
 
 	health "github.com/InVisionApp/go-health"
-	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
-	"github.com/xmidt-org/themis/xlog"
+	"go.uber.org/zap"
 )
 
 // Options holds the available configuration options for the health infrastructure
@@ -23,12 +21,12 @@ type Options struct {
 // New constructs an IHealth instance for the given environment.  If either the DisableLogging option field
 // is set or the given logger is nil, logging will be disabled on the returned health object.  The listener
 // is optional.
-func New(o Options, logger log.Logger, listener health.IStatusListener) (health.IHealth, error) {
+func New(o Options, logger *zap.Logger, listener health.IStatusListener) (health.IHealth, error) {
 	h := health.New()
 	if o.DisableLogging || logger == nil {
 		h.DisableLogging()
 	} else {
-		h.Logger = NewHealthLoggerAdapter(logger)
+		h.Logger = logger
 	}
 
 	h.StatusListener = listener
@@ -36,31 +34,22 @@ func New(o Options, logger log.Logger, listener health.IStatusListener) (health.
 }
 
 // OnStart returns an uber/fx Lifecycle hook for startup
-func OnStart(logger log.Logger, h health.IHealth) func(context.Context) error {
+func OnStart(logger *zap.Logger, h health.IHealth) func(context.Context) error {
 	return func(_ context.Context) error {
-		logger.Log(
-			level.Key(), level.InfoValue(),
-			xlog.MessageKey(), "health service starting",
-		)
+		logger.Info("health service starting")
 
 		return h.Start()
 	}
 }
 
 // OnStop returns an uber/fx Lifecycle hook for shutdown
-func OnStop(logger log.Logger, h health.IHealth) func(context.Context) error {
+func OnStop(logger *zap.Logger, h health.IHealth) func(context.Context) error {
 	return func(_ context.Context) error {
-		logger.Log(
-			level.Key(), level.InfoValue(),
-			xlog.MessageKey(), "health service stopping",
-		)
+		logger.Info("health service stopping")
 
 		err := h.Stop()
 		if errors.Is(err, health.ErrAlreadyStopped) {
-			logger.Log(
-				level.Key(), level.InfoValue(),
-				xlog.MessageKey(), "health service already stopped or not running",
-			)
+			logger.Info("health service already stopped or not running")
 
 			return nil
 		}
