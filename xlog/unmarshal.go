e96b2bb5c36e87b3b99eecacbb5f8f8ec7e1ecb5
Owen Cabalceta <denopink@gmail.com>
2022-10-27 12:27:25 -0400

    update xlog package

--- xlog/unmarshal.go
+++ xlog/unmarshal.go
@@ -3,8 +3,8 @@ package xlog
 import (
 	"github.com/xmidt-org/themis/config"
 
-	"github.com/go-kit/log"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 // LogUnmarshalIn defines the set of dependencies for unmarshalling a go-kit logger
@@ -21,8 +21,8 @@ type LogUnmarshalIn struct {
 
 // Unmarshal returns an uber/fx provider function that handles unmarshalling a logger and emitted it as a component.
 // If a *BufferedPrinter component is present, the unmarshalled logger will be set as that printer's logger.
-func Unmarshal(key string) func(LogUnmarshalIn) (log.Logger, error) {
-	return func(in LogUnmarshalIn) (log.Logger, error) {
+func Unmarshal(key string) func(LogUnmarshalIn) (*zap.Logger, error) {
+	return func(in LogUnmarshalIn) (*zap.Logger, error) {
 		var o Options
 		if err := in.Unmarshaller.UnmarshalKey(key, &o); err != nil {
 			return nil, err
