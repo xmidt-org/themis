d6f8b73a077f04abfe26883912a3b666123379f6
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2022-10-28 20:33:04 -0700

    Remove several unused build files and update the docker images to work.

---
+++ .release/docker/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+set -e
+
+# check arguments for an option that would cause /themis to stop
+# return true if there is one
+_want_help() {
+    local arg
+    for arg; do
+        case "$arg" in
+            -'?'|--help|-v)
+                return 0
+                ;;
+        esac
+    done
+    return 1
+}
+
+_main() {
+    # if command starts with an option, prepend themis
+    if [ "${1:0:1}" = '-' ]; then
+        set -- /themis "$@"
+    fi
+
+    # skip setup if they aren't running /themis or want an option that stops /themis
+    if [ "$1" = '/themis' ] && ! _want_help "$@"; then
+        echo "Entrypoint script for themis Server ${VERSION} started."
+
+        if [ ! -s /etc/themis/themis.yaml ]; then
+            echo "Building out template for file"
+            /bin/spruce merge /tmp/themis_spruce.yaml > /etc/themis/themis.yaml
+        fi
+    fi
+
+    exec "$@"
+}
+
+_main "$@"
